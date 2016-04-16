807b1038a65b8913171b1510b6a1185f1ce4d7f8
yacklebeam <jaketroxel@gmail.com>
2016-04-16 08:55:41 -0700

    Chars now exist in rooms, and can be clicked on or moused over

--- game.js
+++ game.js
@@ -35,7 +35,7 @@
         init: function()
         {
             Ctrl.init();
-            Room.init();
+            GameState.init();
             Screen.init();
             CharacterSet.init();
         },
@@ -60,6 +60,73 @@
         }
     };
 
+    var GameState = 
+    {
+        roomWidth : 3,
+        roomHeight : 3,
+        rooms : [],
+        curRoom: 0,
+        possibleDoors : [],
+        walls: [],
+
+        init: function()
+        {
+            this.curRoom = Math.floor(this.roomWidth * this.roomHeight / 2);
+            this.walls = [
+                [150, 50, 650, 50],
+                [150, 550, 150, 50],
+                [150, 550, 650, 550],
+                [650, 550, 650, 50],
+            ];
+            this.genRooms();
+        },
+
+        genRooms: function()
+        {
+            this.possibleDoors = [
+                [400, 50,  -1 * this.roomWidth, 390, 455],//top
+                [650, 300, 1,                   180, 235],//right
+                [400, 550, this.roomWidth,      390, 15],//bottom
+                [150, 300, -1,                  600, 235]//left
+            ];
+
+            this.rooms = [
+                {},
+                {doors: [0,0,1,0], boxes: [], chars: []},
+                {},
+                {doors: [0,1,0,0], boxes: [], chars: []},
+                {doors: [1,1,1,1], boxes: [], chars: []},
+                {doors: [0,0,0,1], boxes: [], chars: []},
+                {},
+                {doors: [1,0,0,0], boxes: [], chars: []},
+                {},
+            ];
+        },
+
+        draw: function()
+        {
+            for(var i = 0; i < 4; i++)
+            {        
+                var line = this.walls[i];    
+                ctx.beginPath();
+                ctx.strokeStyle = 'orange';
+                ctx.moveTo(line[0],line[1]);
+                ctx.lineTo(line[2],line[3]);
+                ctx.stroke();
+            }
+
+            var theRoom = this.rooms[this.curRoom];
+
+            for(var j = 0; j < 4; j++)
+            {
+                if(theRoom.doors[j] == 1)
+                {
+                    drawCircle(this.possibleDoors[j][0], this.possibleDoors[j][1], 10);
+                }
+            }
+        }
+    };
+
     var Screen =
     {
         init: function()
@@ -76,7 +143,7 @@
             ctx.fillRect(0, 0, Game.width, Game.height);
             ctx.restore();
 
-            Room.draw();
+            GameState.draw();
             Ctrl.draw();
             CharacterSet.draw();
         },
@@ -106,18 +173,22 @@
                 }
             }
 
-            for(var j = 0; j < Room.rooms[Room.curRoom].doors.length; j++)
+            for(var j = 0; j < 4; j++)
             {
-                var dist = getDist(Room.rooms[Room.curRoom].doors[j][0], Room.rooms[Room.curRoom].doors[j][1], CharacterSet.positions[0][0] + 10, CharacterSet.positions[0][1] + 65);
-                console.log(dist);
-                if(dist < 20)
+                if(GameState.rooms[GameState.curRoom].doors[j] == 1)
                 {
-                    var newX = Room.rooms[Room.curRoom].doors[j][3];
-                    var newY = Room.rooms[Room.curRoom].doors[j][4];
-                    Room.curRoom = Room.rooms[Room.curRoom].doors[j][2];
-                    CharacterSet.positions[0] = [newX, newY];
-                    Ctrl.dest = [newX, newY];
-                    break;
+                    var dist = getDist(GameState.possibleDoors[j][0], GameState.possibleDoors[j][1], CharacterSet.positions[0][0] + 10, CharacterSet.positions[0][1] + 65);
+
+                    if(dist < 20)
+                    {
+                        var newX = GameState.possibleDoors[j][3];
+                        var newY = GameState.possibleDoors[j][4];
+                        GameState.curRoom += GameState.possibleDoors[j][2];
+                        CharacterSet.positions[0] = [newX, newY];
+                        Ctrl.dest = [newX, newY];
+                        CharacterSet.sprites[0][4] = GameState.curRoom;
+                        break;
+                    }
                 }
             }
         }
@@ -134,9 +205,10 @@
 
     var CharacterSet =
     {
-        cCount : 2,
+        cCount : 20,
         sprites : [],
         positions: [],
+        selectedChar : 0,
 
         init: function()
         {
@@ -151,12 +223,18 @@
                 var face = getRand(0,4);
                 var shirt = getRand(0,4);
                 var pants = getRand(0,4);
-                var x = 300 + (i * 60);
-                var y = 300;
-
-                if(this.sprites.indexOf([hair, face, shirt, pants]) == -1)
+                var x = getRand(200, 600);
+                var y = getRand(100, 500);
+                var room = getRand(0,7);
+                while(room == 0 || room == 2 || room == 6)
                 {
-                    this.sprites.push([hair, face, shirt, pants]);
+                    room = getRand(0,7);
+                }
+                if(i == 0) room = 4;
+
+                if(this.sprites.indexOf([hair, face, shirt, pants, room]) == -1)
+                {
+                    this.sprites.push([hair, face, shirt, pants, room]);
                     this.positions.push([x, y]);
                 }
                 else
@@ -168,117 +246,60 @@
 
         draw: function()
         {
+            var overChar = false;
             for(var i = this.cCount - 1; i >= 0; i--)
             //for(var i = 0; i >= 0; i--)
             {
                 var char = this.sprites[i];
-                var pos = this.positions[i];
-                var x = pos[0];
-                var y = pos[1];
+                if(char[4] == GameState.curRoom)
+                {
+                    var pos = this.positions[i];
+                    var x = pos[0];
+                    var y = pos[1];
 
-                drawImage(AssetLoader.cSprites, x, y + 45,  20, 20, char[3] * 20, 60,  20, 20);
-                drawImage(AssetLoader.cSprites, x, y + 25,  20, 20, char[2] * 20, 40,  20, 20);
-                drawImage(AssetLoader.cSprites, x, y + 5,   20, 20, char[1] * 20, 20,  20, 20);
-                drawImage(AssetLoader.cSprites, x, y,       20, 20, char[0] * 20, 0,   20, 20);
+                    drawImage(AssetLoader.cSprites, x, y + 45,  20, 20, char[3] * 20, 60,  20, 20);
+                    drawImage(AssetLoader.cSprites, x, y + 25,  20, 20, char[2] * 20, 40,  20, 20);
+                    drawImage(AssetLoader.cSprites, x, y + 5,   20, 20, char[1] * 20, 20,  20, 20);
+                    drawImage(AssetLoader.cSprites, x, y,       20, 20, char[0] * 20, 0,   20, 20);
+
+                    var mX = Ctrl.mousePos[0];
+                    var mY = Ctrl.mousePos[1];
+
+                    if(i != 0)
+                    {
+                        if(mX >= x && mX <= x + 20 && mY >= y && mY <= y + 65)
+                        {
+                            overChar = true;
+                            this.selectedChar = i;
+                            drawText("NAME", x, y-5, 10);
+                        }
+                    }
+                }
             }
+
+            if(!overChar) this.selectedChar = 0;
         }
-    };
-
-    var Room = 
-    {
-        curRoom : 0,
-        walls : [],
-        rooms : [],
-
-        init: function()
-        {
-            this.curRoom = 0;
-
-            this.walls = [
-                [150, 50, 650, 50],
-                [150, 550, 150, 50],
-                [150, 550, 650, 550],
-                [650, 550, 650, 50],
-            ];
-
-            this.rooms = [
-                {doors : [[400, 50, 1, 390, 455],[650, 300, 2, 180, 235],[400, 550, 3, 390, 15],[150, 300, 4, 600, 235]], boxes : []},
-                {doors : [[400, 550, 0, 390, 15]], boxes : []},
-                {doors : [[150, 300, 0, 600, 235]], boxes : []},
-                {doors : [[400, 50, 0, 390, 455]], boxes : []},
-                {doors : [[650, 300, 0, 180, 235]], boxes : []},
-            ];
-        },
-
-        draw: function()
-        {
-            for(var i = 0; i < 4; i++)
-            {        
-                var line = this.walls[i];    
-                ctx.beginPath();
-                ctx.strokeStyle = 'orange';
-                ctx.moveTo(line[0],line[1]);
-                ctx.lineTo(line[2],line[3]);
-                ctx.stroke();
-            }
-
-            var theRoom = this.rooms[this.curRoom];
-
-            for(var j = 0; j < theRoom.doors.length; j++)
-            {
-                var door = theRoom.doors[j];
-                drawCircle(door[0], door[1], 10);
-            }
-        },
     };
 
     var Ctrl = 
     {
         init: function() {
             this.dest = [300,300];
-            window.addEventListener('keydown', this.keyDown, true);
-            window.addEventListener('keyup', this.keyUp, true);
+            this.mousePos = [0,0];
+            //window.addEventListener('keydown', this.keyDown, true);
+            //window.addEventListener('keyup', this.keyUp, true);
+            Game.canvas.addEventListener('mousemove', this.getMouseMove, true);
             Game.canvas.addEventListener('mousedown', this.getMouseClick, true);
         },
 
-        keyDown: function(event) {
-            switch (event.keyCode) {
-                case 87://W
-                    Ctrl.up = true;
-                    break;
-                case 65://A
-                    Ctrl.left = true;
-                    break;
-                case 83://S
-                    Ctrl.down = true;
-                    break;
-                case 68://D
-                    Ctrl.right = true;
-                    break;
-                default:
-                    break;
-            }
-        },
+        getMouseMove: function(event)
+        {
+            var x = event.x;
+            var y = event.y;
+            x -= Game.canvas.offsetLeft;
+            y -= Game.canvas.offsetTop;
 
-        keyUp: function(event) {
-            switch (event.keyCode) {
-                case 87://W
-                    Ctrl.up = false;
-                    break;
-                case 65://A
-                    Ctrl.left = false;
-                    break;
-                case 83://S
-                    Ctrl.down = false;
-                    break;
-                case 68://D
-                    Ctrl.right = false;
-                    break;
-                default:
-                    break;
-
-
-            }
+            Ctrl.mousePos = [x,y];
         },
 
         getMouseClick: function(event)
@@ -289,6 +310,12 @@
             y -= Game.canvas.offsetTop;
 
             if(x >= 150 && x <= 650 && y >= 50 && y <= 550) Ctrl.dest = [x - 10, y - 65];
+            if(CharacterSet.selectedChar > 0)
+            {
+                Ctrl.dest = [   CharacterSet.positions[CharacterSet.selectedChar][0] + 10,
+                                CharacterSet.positions[CharacterSet.selectedChar][1] + 30
+                            ];
+            }
         },
 
         draw: function()
@@ -325,6 +352,13 @@
         var xd = x1 - x2;
         var yd = y1 - y2;
         return Math.sqrt(xd * xd + yd * yd);
+    }
+
+    function drawText(text, x, y, size)
+    {
+        ctx.fillStyle = '#FFFFFF';
+        ctx.font = size + "px Arial";
+        ctx.fillText(text, x, y);
     }
 
     window.onload = function()
